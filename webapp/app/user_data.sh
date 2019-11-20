cf53a614f9f11a3915acc0c869d13f074f4c5ddf
Sebastian Maniak <1268277+sebbycorp@users.noreply.github.com>
2019-11-20 14:02:35 -0500

    update

--- webapp/app/user_data.sh
+++ webapp/app/user_data.sh
@@ -4,8 +4,8 @@ yum -y install httpd php mysql php-mysql
 chkconfig httpd on
 service httpd start
 cd /var/www/html
-S3_HOST=s3-us-west-2.amazonaws.com
-APP_PATH=us-west-2-aws-training/awsu-spl/spl-13/scripts/app.tgz
+S3_HOST=s3-us-east-1.amazonaws.com
+APP_PATH=us-east-1-aws-training/awsu-spl/spl-13/scripts/app.tgz
 wget https://${S3_HOST}/${APP_PATH}
 tar xvfz app.tgz
 chown apache:root /var/www/html/rds.conf.php
